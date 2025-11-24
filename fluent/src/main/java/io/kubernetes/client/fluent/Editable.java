8965d3bf9b1bd710f67790f36fada47e718cd991
Michel <michel.philippe@festijazz.be>
2025-11-24 16:37:40 +0100

    Updated forked repo

--- fluent/src/main/java/io/kubernetes/client/fluent/Editable.java
+++
@@ -1,9 +0,0 @@
-package io.kubernetes.client.fluent;
-
-public interface Editable<T>{
-  
-  
-  T edit();
-  
-
-}
--- fluent/src/main/java/io/kubernetes/client/fluent/Nested.java
+++
@@ -1,9 +0,0 @@
-package io.kubernetes.client.fluent;
-
-public interface Nested<F>{
-  
-  
-  F and();
-  
-
-}
